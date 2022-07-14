6f701c5e763d38ce3fc13bba5740a8134d5f3f7d
Kayvan Javid <javid.kayvan@gmail.com>
2022-07-14 23:58:23 +0000

    Added second working sub command cpu
    Signed-off-by: Kayvan Javid <javid.kayvan@gmail.com>

---
+++ cmd/cpu.go
@@ -0,0 +1,67 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/shirou/gopsutil/cpu"
+)
+
+const (
+	infoFlag = "info"
+	topFlag = "top"
+)
+
+var info, top bool
+
+var cpuCmd = createCPUCmd()
+
+func createCPUCmd() *cobra.Command {
+	return &cobra.Command {
+		Use:   "cpu",
+		Short: "Provides CPU information",
+		Long: `
+For those occasions when all of the decades worth of established,
+reliable and convenient CPU info and utilization tools are inconveniently not available.
+`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.Flags().Visit(func(flag *pflag.Flag) {
+				//fmt.Printf("Flag supplied %v: %v\n", flag.Name, flag.Value)
+				switch flag.Name {
+					case "info":
+						fmt.Println("Retrieving CPU hardware info")
+						cpuInfo()
+					case "top":
+						fmt.Println("Retrieving CPU utilization info")
+						topInfo()
+					default:
+						fmt.Println("Nothing to do - neither info nor top supplied")
+				}
+			})
+			return nil
+		},
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(cpuCmd)
+
+	// flags and their configuration
+	cpuCmd.Flags().Bool(infoFlag, false, "Obtains CPU hardware info")
+	cpuCmd.Flags().Bool(topFlag, false, "Obtains CPU utilization info")
+}
+
+func cpuInfo() {
+	output, _ := cpu.Info()
+	spew.Dump(output)
+}
+
+func topInfo() {
+	output, _ := cpu.Times(true)
+	spew.Dump(output)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/k4vej/alta3research-go-cert
 go 1.16
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
 )
